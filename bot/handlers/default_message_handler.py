dfe28b4348ed04ad186c1127f11d4f2eee116fec
prxpostern <44132439+prxpostern@users.noreply.github.com>
2021-10-06 15:06:04 +0330

    Update default_message_handler.py

--- bot/handlers/default_message_handler.py
+++ bot/handlers/default_message_handler.py
@@ -4,7 +4,8 @@ from bot.handlers import leech_handler
 
 @Client.on_message(Filters.private & Filters.command(COMMAND.LEECH))
 async def func(client, update):
-  cmd, file_name = update.text.split(" ", 1)
+  if (" " in update.text) and (update.reply_to_message is not None):
+    cmd, file_name = update.text.split(" ", 1)
   message = update.reply_to_message
   message.text = "/" + COMMAND.LEECH + " " + message.text
   return await leech_handler.func(client, message)
